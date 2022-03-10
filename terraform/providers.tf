f370494a261b9a34ac6e5463dba517b7f5a324fe
karthikvemula <44648255+karthikvemula@users.noreply.github.com>
2022-03-10 12:41:46 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {}
+
+# Add a user to the organization
+resource "github_membership" "membership_for_user_x" {
+  # ...
+}
 /*terraform {
   required_providers {
     azurerm = {
