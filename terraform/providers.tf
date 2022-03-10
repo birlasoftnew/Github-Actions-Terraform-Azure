76a08db1e4febcb0a291c19542710fc89ef461ec
karthikvemula <44648255+karthikvemula@users.noreply.github.com>
2022-03-10 12:46:55 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,7 +11,7 @@ terraform {
 provider "github" {}
 
 # Add a user to the organization
-resource "github_membership" "membership_for_user_x" {
+#resource "github_membership" "membership_for_user_x" {
   # ...
 }
 /*terraform {
