2d0aeb2e3e53de177f45c08f96de185ca8b99727
karthikvemula <44648255+karthikvemula@users.noreply.github.com>
2022-03-11 19:15:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "github_repository" "aigi-infra-iac-github" {
-  name        = "${var.reponame}"
+  name        = var.reponame
   description = "My awesome codebase"
   #auto_init        = "true"
 
