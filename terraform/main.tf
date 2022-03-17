fb51ef503483766489a26b5bf86b4b0159fd8552
karthikvemula <44648255+karthikvemula@users.noreply.github.com>
2022-03-17 13:05:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,15 @@
-resource "github_repository" "aigi-infra-iac-github" {
-  name        = "${var.reponame}"
-  description = "My awesome codebase"
-  #auto_init        = "true"
-
-  visibility = "public"
-
-  /*template {
-    owner      = "github"
-    repository = "jenkins"
-  }*/
-}
-
-/*terraform {
-  backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
-    key                  = "dev.terraform.tfstate"
+resource "docker_image" "jfrog" {
+  name = "jfrog"
+  build {
+    path = "."
+    tag  = ["jfrog:develop"]
+    dockerfile = Dockerfile
+    #(String) Name of the Dockerfile. Defaults to Dockerfile.
+  /*  build_arg = {
+      foo : "zoo"
+    }
+    label = {
+      author : "zoo"
+    }*/
   }
 }
-module "RG" {
-  source   = "./modules/RG" #A
-  rgname   = var.rgname     #B
-  location = var.location
-}
-module "SA" {
-  source   = "./modules/StorageAccount"
-  sname    = var.sname
-  rgname   = var.rgname
-  location = var.location
-}*/
