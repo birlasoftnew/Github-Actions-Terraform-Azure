5a7f7862d79008390955c2dd8f93ab8684e00472
karthikvemula <44648255+karthikvemula@users.noreply.github.com>
2022-03-17 13:08:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "docker_image" "jfrog" {
   build {
     path = "."
     tag  = ["jfrog:develop"]
-    dockerfile = Dockerfile
+    dockerfile = "Dockerfile"
     #(String) Name of the Dockerfile. Defaults to Dockerfile.
   /*  build_arg = {
       foo : "zoo"
